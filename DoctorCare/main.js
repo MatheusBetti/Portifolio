8f5a18eca32a48616831abde67616f40f43434fa
Matheus Betti <matheusbbetti@gmail.com>
2022-05-09 12:36:54 -0300

    Projeto_DoctorCare

---
+++ DoctorCare/main.js
@@ -0,0 +1,48 @@
+window.addEventListener('scroll', onScroll)
+onScroll();
+
+function onScroll(){
+    showNavOnScroll()
+    showBackToTopButtonOnScroll()
+}
+
+function showNavOnScroll(){
+    if (scrollY > 0){
+        navigation.classList.add('scroll')
+    }
+    else{
+        navigation.classList.remove('scroll');
+    }
+}
+
+function showBackToTopButtonOnScroll(){
+    if (scrollY > 1450){
+        backToTopButton.classList.add('show')
+    }
+    else{
+        backToTopButton.classList.remove('show');
+    }
+}
+
+function openMenu(){
+    document.body.classList.add('menu-expanded')
+}
+
+function closeMenu(){
+    document.body.classList.remove('menu-expanded')
+}
+
+ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 700,
+}).reveal(`
+#home, 
+#home img, 
+#home .stats,
+#services,
+#services header,
+#services .card,
+#about,
+#about header,
+#about content`);

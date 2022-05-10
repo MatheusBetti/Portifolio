2ca29ff62a8f88cd27660cbf583a66e8f73b7e21
Matheus Betti <matheusbbetti@gmail.com>
2022-05-10 15:45:19 -0300

    AtualizaçãoDESKTOP

--- DoctorCare/main.js
+++ DoctorCare/main.js
@@ -4,7 +4,36 @@ onScroll();
 function onScroll(){
     showNavOnScroll()
     showBackToTopButtonOnScroll()
+
+    activateMenuAtCurrentSection(home)
+    activateMenuAtCurrentSection(services)
+    activateMenuAtCurrentSection(about)
+    activateMenuAtCurrentSection(contact)
 }
+
+function activateMenuAtCurrentSection(section){
+    const targetLine = scrollY + (innerHeight/2)
+
+    //Verificação se seção passou da linha
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop
+
+    //Verificar se a base está abaixo da linha alvo
+    const sectionEndsAt = sectionTop + sectionHeight
+    const sectionEndPassedTargetline = sectionEndsAt <= targetLine
+
+    //Query no ID da section
+    const menuElement = document.querySelector(`.menu a[href*=${section.getAttribute('id')}]`)
+
+    //Remover para quando sair da section
+    menuElement.classList.remove('active')
+    
+   if (sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetline){
+       menuElement.classList.add('active')
+   }
+}
+
 
 function showNavOnScroll(){
     if (scrollY > 0){
